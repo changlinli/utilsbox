cf058d5ac7cb04ebe9043cd9f8b65cc1eb0d6773
Changlin Li <mail@changlinli.com>
2017-01-18 22:34:42 -0500

    Add silenceAndFeed interpreter
    This adds a pure interpreter to the teletype effect.

--- System/UtilsBoxSpec/Teletype.hs
+++ System/UtilsBoxSpec/Teletype.hs
@@ -30,3 +30,31 @@ getLine = do
     if c == '\n'
        then return ""
        else getLine >>= \line -> return (c : line)
+
+putStrNew :: (F.MonadFree f g, TeletypeAPI :<: f) => String -> g ()
+putStrNew x = F.liftF . inject $ PutStr x ()
+
+getCharNew :: (F.MonadFree f g, TeletypeAPI :<: f) => g Char
+getCharNew = F.liftF . inject $ GetChar id
+
+silence :: (F.MonadFree TeletypeAPI g) => TeletypeAPI a -> g a
+silence (PutStr _ next) = return next
+silence x @ (GetChar _) = F.liftF x
+
+feed :: (F.MonadFree TeletypeAPI g) => Char -> TeletypeAPI a -> g a
+feed _ x @ (PutStr _ _) = F.liftF x
+feed char (GetChar f) = return (f char)
+
+silenceAndFeed :: Char -> TeletypeAPI a -> a
+silenceAndFeed _ (PutStr _ next) = next
+silenceAndFeed char (GetChar f) = f char
+
+example :: (F.MonadFree TeletypeAPI g) => g String
+example = do
+    x <- getCharNew
+    putStrNew [x]
+    y <- getCharNew
+    putStrNew [y]
+    return [x, y]
+
+exampleExec = F.iterA (silenceAndFeed 'c') example
