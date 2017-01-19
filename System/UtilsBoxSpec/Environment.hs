6bf8d0aa27016e9201c2a4ea91877f5685ea74ef
Changlin Li <mail@changlinli.com>
2017-01-18 20:57:18 -0500

    Add new pure interpreter for EnvironmentAPI
    Pass in a pure map and allow the user to manipulate the map.

--- System/UtilsBoxSpec/Environment.hs
+++ System/UtilsBoxSpec/Environment.hs
@@ -6,7 +6,9 @@ module System.UtilsBoxSpec.Environment where
 
 import qualified Control.Monad.Free as F
 import           System.UtilsBoxSpec.CoreTypes ((:<:), inject)
+import           Data.Functor.Identity (Identity (..))
 import qualified Data.Map as M
+import           Data.Maybe (fromMaybe)
 import qualified System.Environment as SE
 
 data EnvironmentAPI next 
@@ -25,3 +27,12 @@ environmentIOF :: EnvironmentAPI a -> IO a
 environmentIOF (GetArgs next) = SE.getArgs >>= (fmap return next)
 environmentIOF (GetProgName next) = SE.getProgName >>= (fmap return next)
 environmentIOF (GetEnvironment next) = fmap M.fromList SE.getEnvironment >>= (fmap return next)
+
+environmentPure :: M.Map String String -> EnvironmentAPI a -> Identity a
+environmentPure keyValues x = case x of
+                                   (GetArgs next) -> Identity arguments >>= (fmap return next)
+                                   (GetProgName next) -> Identity programName >>= (fmap return next)
+                                   (GetEnvironment next) -> Identity keyValues >>= (fmap return next)
+  where
+    arguments = (fromMaybe []) . (fmap words) $ M.lookup "arguments" keyValues
+    programName = fromMaybe "" $ M.lookup "programName" keyValues
